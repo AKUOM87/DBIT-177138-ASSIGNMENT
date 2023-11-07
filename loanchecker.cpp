878ae37cb38550800ead83a28b171bd6c325dcb3
Victor Akuom <vtaku32@gmail.com>
2023-11-07 14:59:12 +0000

    bash file

--- loanchecker.cpp
+++ loanchecker.cpp
@@ -1,2 +1,10 @@
-#include <iostrean>
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+    cout << "Hello World!\n";
+
+    return 0;
+}
---
+++ run_loanchecker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+g++ loanchecker.cpp
+./a.out
